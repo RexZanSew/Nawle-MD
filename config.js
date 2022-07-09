a7a601feb38729ebcd5ed97dc3dd809c6e0728c5
RexZanSew <108753739+RexZanSew@users.noreply.github.com>
2022-07-10 01:06:25 +0700

    config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6282117305255'],
-  ['6282117305255'],
-  ['6282117305255', 'Owner', true]
+  ['6285321524846'],
+  ['6285321524846'],
+  ['6285321524846', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
